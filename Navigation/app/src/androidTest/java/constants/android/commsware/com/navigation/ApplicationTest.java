c397bd5d2886f23efcb0bb750aecbc9d8d1396fb
Yong <yong.nousco@gmail.com>
2015-01-29 17:50:42 +0900

    [Yong] Base layout Upload

---
+++ Navigation/app/src/androidTest/java/constants/android/commsware/com/navigation/ApplicationTest.java
@@ -0,0 +1,13 @@
+package constants.android.commsware.com.navigation;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
