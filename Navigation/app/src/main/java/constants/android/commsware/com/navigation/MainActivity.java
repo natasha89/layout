2e45b62db23f74127e2d585807e2c0851a2a4c5c
Yong-tf <yong.nousco@gmail.com>
2015-02-02 10:25:09 +0900

    [Yong] merge Update

--- Navigation/app/src/main/java/constants/android/commsware/com/navigation/MainActivity.java
+++ Navigation/app/src/main/java/constants/android/commsware/com/navigation/MainActivity.java
@@ -1,26 +1,25 @@
 package constants.android.commsware.com.navigation;
 
-import android.app.Activity;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.media.Image;
-import android.os.AsyncTask;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.TypedArray;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.Gravity;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -30,21 +29,18 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import DTO.NavDrawerItem;
 import DTO.Record;
 
 
 public class MainActivity extends ActionBarActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+        implements AdapterView.OnItemClickListener {
 
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
-    private NavigationDrawerFragment mNavigationDrawerFragment;
-
-    /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
-     */
-    private CharSequence mTitle;
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private ListView mLvDrawerMenu;
+    private NavDrawerListAdapter mDrawerMenuAdapter;
 
     ArrayList<Record> mRecords = new ArrayList<Record>();
 
@@ -56,12 +52,9 @@ public class MainActivity extends ActionBarActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
-
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp(R.id.navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout));
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mLvDrawerMenu = (ListView) findViewById(R.id.list_slidermenu);
 
         //String userName, String rivalName, String runningDate, String endTime, boolean isWin
         mRecords.add(new Record("A", "Foxy", new SimpleDateFormat("yyyy MM dd").format(new Date()), new SimpleDateFormat("HH mm").format(new Date()), true));
@@ -69,110 +62,129 @@ public class MainActivity extends ActionBarActivity
         mRecords.add(new Record("A", "Red eyes", new SimpleDateFormat("yyyy MM dd").format(new Date()), new SimpleDateFormat("HH mm").format(new Date()), false));
         mRecords.add(new Record("", "Foxy", new SimpleDateFormat("yyyy MM dd").format(new Date()), new SimpleDateFormat("HH mm").format(new Date()), false));
 
-        mListTimeLine = (ListView)findViewById(R.id.list_timeline);
+        mListTimeLine = (ListView) findViewById(R.id.list_timeline);
         mTimelineAdapter = new RecordAdapter(this, R.id.list_timeline, mRecords);
         mListTimeLine.setAdapter(mTimelineAdapter);
+
+        mToolbar.setLogo(R.drawable.abc_btn_check_material);
+        mToolbar.setTitle("Main");
+        mToolbar.setSubtitle("Sub title");
+        //setSupportActionBar(mToolbar); // Show settings.
+
+        List<NavDrawerItem> menuItems = generateDrawerMenuItems();
+        mDrawerMenuAdapter = new NavDrawerListAdapter(getApplicationContext(), menuItems);
+        mLvDrawerMenu.setAdapter(mDrawerMenuAdapter);
+
+        mLvDrawerMenu.setOnItemClickListener(this);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.app_name, R.string.app_name) {
+            public void onDrawerClosed(View view) {
+                mToolbar.setTitle("Main");
+                mToolbar.setSubtitle("Sub title");
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                mToolbar.setTitle("Open ToolBar");
+                invalidateOptionsMenu();
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        if (savedInstanceState == null) {
+            setFragment(0, BikeFragment.class);
+        }
+
     }
 
     @Override
-    public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
-    }
-
-    public void onSectionAttached(int number) {
-        switch (number) {
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+            case 0:
+                setFragment(0, BikeFragment.class);
+                break;
             case 1:
-                mTitle = getString(R.string.title_section1);
+                setFragment(1, BusFragment.class);
                 break;
             case 2:
-                mTitle = getString(R.string.title_section2);
+                setFragment(2, CarFragment.class);
                 break;
             case 3:
-                mTitle = getString(R.string.title_section3);
+                mDrawerLayout.closeDrawer(mLvDrawerMenu);
+                mLvDrawerMenu.invalidateViews();
+                break;
+            case 4:
+                mDrawerLayout.closeDrawer(mLvDrawerMenu);
+                mLvDrawerMenu.invalidateViews();
+                break;
+            case 5:
+                mDrawerLayout.closeDrawer(mLvDrawerMenu);
+                mLvDrawerMenu.invalidateViews();
                 break;
         }
     }
 
-    public void restoreActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
-    }
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.main, menu);
-            restoreActionBar();
-            return true;
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(mLvDrawerMenu)) {
+            mDrawerLayout.closeDrawer(mLvDrawerMenu);
+        } else {
+            super.onBackPressed();
         }
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     /**
-     * A placeholder fragment containing a simple view.
+     * When using the ActionBarDrawerToggle, you must call it during
+     * onPostCreate() and onConfigurationChanged()...
      */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    public void setFragment(int position, Class<? extends Fragment> fragmentClass) {
+        try {
+            Fragment fragment = fragmentClass.newInstance();
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container, fragment, fragmentClass.getSimpleName());
+            fragmentTransaction.commit();
+
+            mLvDrawerMenu.setItemChecked(position, true);
+            mDrawerLayout.closeDrawer(mLvDrawerMenu);
+            mLvDrawerMenu.invalidateViews();
+        } catch (Exception ex) {
+            Log.e("setFragment", ex.getMessage());
         }
+    }
 
-        public PlaceholderFragment() {
+    //Drawer Menu Items
+    private List<NavDrawerItem> generateDrawerMenuItems() {
+        String[] itemsText = getResources().getStringArray(R.array.nav_drawer_items);
+        TypedArray itemsIcon = getResources().obtainTypedArray(R.array.nav_drawer_icons);
+        List<NavDrawerItem> result = new ArrayList<NavDrawerItem>();
+        for (int i = 0; i < itemsText.length; i++) {
+            NavDrawerItem item = new NavDrawerItem();
+            item.setText(itemsText[i]);
+            item.setIcon(itemsIcon.getResourceId(i, -1));
+            result.add(item);
         }
+        return result;
+    }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-
-
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
     }
 
     //for Timeline CustomListAdapter
@@ -189,65 +201,32 @@ public class MainActivity extends ActionBarActivity
             this.mItems = items;
         }
 
-        //for customAdapter extends BaseAdapter
-        //public int getCount() {
-        //    return mItems.size();
-        //}
-
-        //public Record getItem(int position) {
-        //    return mItems.get(position);
-        //}
-
-        //public long getItemId(int position) {
-        //    return position;
-        //}
 
         public View getView(int position, View convertView, ViewGroup parent) {
             View row = convertView;
             ViewHolder mHholder = null;
 
             if (row == null) {
-                LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 row = inflater.inflate(R.layout.item_record_list, null);
 
                 mHholder = new ViewHolder();
-                mHholder.pic = (ImageView)row.findViewById(R.id.img_record_pic);
-                mHholder.date = (TextView)row.findViewById(R.id.text_record_date);
-                mHholder.time = (TextView)row.findViewById(R.id.text_record_time);
-                mHholder.isNew = (TextView)row.findViewById(R.id.text_record_new);
-                mHholder.result = (TextView)row.findViewById(R.id.text_record_result);
+                mHholder.pic = (ImageView) row.findViewById(R.id.img_record_pic);
+                mHholder.date = (TextView) row.findViewById(R.id.text_record_date);
+                mHholder.time = (TextView) row.findViewById(R.id.text_record_time);
+                mHholder.isNew = (TextView) row.findViewById(R.id.text_record_new);
+                mHholder.result = (TextView) row.findViewById(R.id.text_record_result);
 
                 row.setTag(mHholder);
+            } else {
+                mHholder = (ViewHolder) row.getTag();
             }
-            else { mHholder = (ViewHolder)row.getTag(); }
 
             //holder.date.setText("");
 
             return row;
         }
 
-        //picture loaing type?
-        //private static class ThumbnailTask extends AsyncTask<Void, Void, Cursor> {
-        //    private int mPosition;
-        //    private ViewHolder mHolder;
-
-        //    public ThumbnailTask(int position, ViewHolder holder) {
-                mPosition = position;
-                mHolder = holder;
-        //    }
-
-            protected Cursor doInBackground(Void... arg0) {
-//                Glide.with(GlideFragment.this)
-//                        .load("https://raw.githubusercontent.com/bumptech/glide/master/static/glide_logo.png")
-//                        .into(imgGlide);
-            }
-
-            protected void onPostExcute(Bitmap bitmap) {
-                if (mHolder.position == mPosition)
-                    mHolder.pic.setImageBitmap(bitmap);
-
-            }
-        }
     }
 
     private static class ViewHolder {
